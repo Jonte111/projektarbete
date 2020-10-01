1552b3da14f1058fd790b21eaff383b870023554
Jonte111 <jonathan.pe@telia.com>
2020-10-01 13:03:43 +0200

    Made the start of commandLineApp.

--- src/projektMain/Main.java
+++ src/projektMain/Main.java
@@ -1,7 +1,46 @@
 package projektMain;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Player p1 = new Player("Bob", 1000);
+        Scanner scan = new Scanner(System.in);
+        //commandLineApp
+        System.out.println("How many players do you want?");
+        int numberOfPlayers = scan.nextInt();
+        System.out.println("How many rounds?");
+        int numberOfRounds= scan.nextInt();
+
+        switch(numberOfPlayers){
+            case 1 -> {
+                System.out.println("En spelare");
+                Player p1 = new Player("Player1", 1000);
+                new Game();
+            }
+            case 2 ->{
+                System.out.println("Två spelare");
+                Player p1 = new Player("Player1", 1000);
+                Player p2 = new Player("Player2", 1000);
+                new Game();
+            }
+            case 3 ->{
+                System.out.println("Tre spelare");
+                Player p1 = new Player("Player1", 1000);
+                Player p2 = new Player("Player2", 1000);
+                Player p3 = new Player("Player3", 1000);
+                new Game();
+            }
+            case 4 -> {
+                System.out.println("Fyra spelare");
+                Player p1 = new Player("Player1", 1000);
+                Player p2 = new Player("Player2", 1000);
+                Player p3 = new Player("Player3", 1000);
+                Player p4 = new Player("Player4", 1000);
+                new Game();
+            }
+
+        }
+
+
     }
 }
