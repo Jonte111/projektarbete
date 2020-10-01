1a4438c186de1c1803040f57560698234041f5db
Jonte111 <jonathan.pe@telia.com>
2020-10-01 12:28:15 +0200

    Made constructor for Animal.

--- src/projektMain/Animal.java
+++ src/projektMain/Animal.java
@@ -1,7 +1,14 @@
 package projektMain;
 
 public abstract class Animal {
+    private String name;
+    private String gender;
+    private int health;
 
-    //public animal()
+    public Animal(String name, String gender, int health){
+        this.name = name;
+        this.gender=gender;
+        this.health=health;
+    }
 
 }
