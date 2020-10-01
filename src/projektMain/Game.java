c9a57154d11a99c15bfc3ad76ad28cc28c5f34e8
Jonte111 <jonathan.pe@telia.com>
2020-10-01 13:22:18 +0200

    Made main smaller and moved methods to Game and made a dynamic method that creates number of players and number of rounds needed.

--- src/projektMain/Game.java
+++ src/projektMain/Game.java
@@ -1,4 +1,27 @@
 package projektMain;
 
 public class Game {
+
+    public Game(int numberOfPlayers, int numberOfRounds){
+        int num = 1;
+        while(numberOfPlayers > 0){
+            new Player("Player"+num, 1000);
+            num++;
+            numberOfPlayers--;
+
+        }
+
+        while(numberOfRounds > 0){
+
+
+
+
+
+            numberOfRounds--;
+        }
+
+
+
+    }
+
 }
--- src/projektMain/Main.java
+++ src/projektMain/Main.java
@@ -14,29 +14,29 @@ public class Main {
         switch(numberOfPlayers){
             case 1 -> {
                 System.out.println("En spelare");
-                Player p1 = new Player("Player1", 1000);
-                new Game();
+                //Player p1 = new Player("Player1", 1000);
+                new Game(1,numberOfRounds);
             }
             case 2 ->{
                 System.out.println("Två spelare");
-                Player p1 = new Player("Player1", 1000);
-                Player p2 = new Player("Player2", 1000);
-                new Game();
+                //Player p1 = new Player("Player1", 1000);
+                //Player p2 = new Player("Player2", 1000);
+                new Game(2,numberOfRounds);
             }
             case 3 ->{
                 System.out.println("Tre spelare");
-                Player p1 = new Player("Player1", 1000);
-                Player p2 = new Player("Player2", 1000);
-                Player p3 = new Player("Player3", 1000);
-                new Game();
+                //Player p1 = new Player("Player1", 1000);
+                //Player p2 = new Player("Player2", 1000);
+                //Player p3 = new Player("Player3", 1000);
+                new Game(3,numberOfRounds);
             }
             case 4 -> {
                 System.out.println("Fyra spelare");
-                Player p1 = new Player("Player1", 1000);
-                Player p2 = new Player("Player2", 1000);
-                Player p3 = new Player("Player3", 1000);
-                Player p4 = new Player("Player4", 1000);
-                new Game();
+                //Player p1 = new Player("Player1", 1000);
+                //Player p2 = new Player("Player2", 1000);
+                //Player p3 = new Player("Player3", 1000);
+                //Player p4 = new Player("Player4", 1000);
+                new Game(4,numberOfRounds);
             }
 
         }
