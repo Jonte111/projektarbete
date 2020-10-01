109636c4dcc267dce40ad67a6734c64834c26ad7
Jonte111 <jonathan.pe@telia.com>
2020-10-01 12:12:57 +0200

    Made constructor for Player.

--- src/projektMain/Player.java
+++ src/projektMain/Player.java
@@ -1,4 +1,11 @@
 package projektMain;
 
 public class Player {
+    private String name;
+    private int money;
+
+    public void player(String name, int money){
+        this.name = name;
+        this.money = money;
+    }
 }
