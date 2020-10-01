b6f6727a52abe3b814f24e0d0d6a4d79fcc6d9bb
Jonte111 <jonathan.pe@telia.com>
2020-10-01 13:59:33 +0200

    Added arrayList to Player Constructor

--- src/projektMain/Player.java
+++ src/projektMain/Player.java
@@ -1,10 +1,14 @@
 package projektMain;
 
+import java.util.ArrayList;
+
 public class Player {
     private String name;
     private int money;
-
-    public Player(String name, int money) {
+    private ArrayList<Animal> playerOwnedAnimals;
+    //Player needs a ArrayList to store animals and food
+    //Hashtable<food, quantity>;
+    public Player(String name, int money, ArrayList<Animal> playerOwnedAnimals) {
         this.name = name;
         this.money = money;
     }
