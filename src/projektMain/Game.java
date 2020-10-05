1395ba7af2631797702633fff55c6157714e15bc
Jonte111 <jonathan.pe@telia.com>
2020-10-05 08:58:30 +0200

    Added a scanner. Made user menu. Made if skeleton for the game.

--- src/projektMain/Game.java
+++ src/projektMain/Game.java
@@ -1,10 +1,12 @@
 package projektMain;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Game {
     ArrayList<Player> players = new ArrayList<Player>();
     public Game(int numberOfPlayers, int numberOfRounds){
+        Scanner scan = new Scanner(System.in);
         int playerNum = 1;
         while(numberOfPlayers > 0){
             players.add( new Player(playerNum, 1000, null));
@@ -13,10 +15,26 @@ public class Game {
         }
         //Playing the rounds
         while(numberOfRounds > 0){
-        for(var pointer: players){
-            System.out.println("Name of the player");
-            System.out.println(pointer.name);
-        }
+            for(var pointer: players){
+                System.out.println("Name of the player");
+                System.out.println(pointer.name);
+                System.out.println("Vad vill du göra?:\n" +
+                        "Köpa ett djur(1)\n" +
+                        "Köpa foder(2)\n" +
+                        "Para djuren(3)");
+                int choice = scan.nextInt();
+                if(choice==1){
+
+                }else if(choice==2){
+
+                }else if(choice==3){
+
+                }else{
+                    System.out.println("Du skrev något som inte fanns");
+                    continue;
+                }
+
+            }
 
             numberOfRounds--;
         }
