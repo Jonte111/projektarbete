27559d86cc3a20a6e04895b52847de9b5db28172
Jonte111 <jonathan.pe@telia.com>
2020-10-06 11:13:04 +0200

    Made case 1 Cat, you can add name and gender.

--- src/projektMain/Store.java
+++ src/projektMain/Store.java
@@ -1,12 +1,26 @@
 package projektMain;
 
+import java.util.Scanner;
+enum gender{
+    MALE,
+    FEMALE
+}
+
 public class Store {
 
     public Store(int animalType){
+        Scanner scanInt = new Scanner(System.in);
+        Scanner scanLine = new Scanner(System.in);
 
+        String name;
+        String gender;
         switch(animalType){
            case 1 ->{
-               new Cat();
+               System.out.println("Vad vill du döpa katten till?");
+               name=scanInt.nextLine();
+               System.out.println("Vilket kön? male famale");
+               gender = scanLine.nextLine().toUpperCase();
+               new Cat(name,gender);
            }
            case 2 ->{
                new Dog();
