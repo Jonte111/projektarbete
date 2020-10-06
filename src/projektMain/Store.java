ad736893d705b4371091825bfce9098b27e98341
Jonte111 <jonathan.pe@telia.com>
2020-10-06 10:31:49 +0200

    Added switch in Store to determine animal class you want to buy. Realised that Animal can not contain name, it must be in the animal classes like Cat, Dog etc.

--- src/projektMain/Store.java
+++ src/projektMain/Store.java
@@ -4,6 +4,27 @@ public class Store {
 
     public Store(int animalType){
 
+        switch(animalType){
+           case 1 ->{
+               new Cat();
+           }
+           case 2 ->{
+               new Dog();
+           }
+           case 3 ->{
+               new Bird();
+           }
+           case 4 ->{
+               new Fish();
+           }
+           case 5 ->{
+               new Horse();
+           }
+
+
+        }
+
+
     }
 
 }
